8feb150b608f04e17229b34e8b78b6cdaee95a96
joli94 <2558817+joli94@users.noreply.github.com>
2022-05-08 21:37:55 +0300

    Added finAll via sql union

--- src/main/java/ro/unibuc/fmi/dietapp/repository/UserRepository.java
+++ src/main/java/ro/unibuc/fmi/dietapp/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package ro.unibuc.fmi.dietapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ro.unibuc.fmi.dietapp.model.User;
 
@@ -9,6 +10,10 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    @Query(value = "SELECT  * FROM users@bd_eaeu UNION ALL SELECT * FROM users@bd_weeu", nativeQuery = true)
+    //TODO: de pus sinonimele
+    List<User> findAll();
+
     List<User> findByCityId(Long id);
 
     Optional<User> findByUsername(String username);
