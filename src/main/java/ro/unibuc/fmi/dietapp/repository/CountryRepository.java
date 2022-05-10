23bdc2267f78c0fe83d456d02c01478fb0008073
joli94 <2558817+joli94@users.noreply.github.com>
2022-05-10 14:28:10 +0300

    Added synonims in country

--- src/main/java/ro/unibuc/fmi/dietapp/repository/CountryRepository.java
+++ src/main/java/ro/unibuc/fmi/dietapp/repository/CountryRepository.java
@@ -2,15 +2,19 @@ package ro.unibuc.fmi.dietapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ro.unibuc.fmi.dietapp.model.Country;
 import ro.unibuc.fmi.dietapp.model.User;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CountryRepository extends JpaRepository<Country, Long> {
-    @Query(value = "SELECT * FROM country_eaeu@bd_eaeu UNION ALL SELECT * FROM country_weeu@bd_weeu", nativeQuery = true)
-        //TODO: de pus sinonimele
+    @Query(value = "SELECT * FROM country_eaeu UNION ALL SELECT * FROM country_weeu", nativeQuery = true)
     List<Country> findAll();
+
+    @Query(value = "SELECT * FROM (SELECT * FROM country_eaeu UNION ALL SELECT * FROM country_weeu) WHERE country_id = ?", nativeQuery = true)
+    Optional<Country> findById(Long id);
 }
