20e5a20d207e38002f6a36cb23e8919a0066a5d6
joli94 <2558817+joli94@users.noreply.github.com>
2022-05-10 14:44:30 +0300

    Added weights, with synonyms

--- src/main/java/ro/unibuc/fmi/dietapp/repository/WeightRepository.java
+++ src/main/java/ro/unibuc/fmi/dietapp/repository/WeightRepository.java
@@ -1,12 +1,23 @@
 package ro.unibuc.fmi.dietapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ro.unibuc.fmi.dietapp.model.Weight;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface WeightRepository extends JpaRepository<Weight, Long> {
+    @Query(value = "SELECT * FROM weight_eaeu UNION ALL SELECT * FROM weight_weeu", nativeQuery = true)
+    List<Weight> findAll();
+
+    @Query(value = "SELECT * FROM (SELECT * FROM weight_eaeu UNION ALL SELECT * FROM weight_weeu) WHERE user_user_id = ?", nativeQuery = true)
     List<Weight> findByUserId(Long id);
+
+    @Query(value = "SELECT * FROM (SELECT * FROM weight_eaeu UNION ALL SELECT * FROM weight_weeu) WHERE weight_id = ?", nativeQuery = true)
+    Optional<Weight> findById(Long id);
+
+    //TODO: create, update
 }
