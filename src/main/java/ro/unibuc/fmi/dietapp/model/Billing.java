28e633f53586028833002dfce29a4c172d80c36f
joli94 <2558817+joli94@users.noreply.github.com>
2022-05-11 23:35:11 +0300

    Resolved billing Error

--- src/main/java/ro/unibuc/fmi/dietapp/model/Billing.java
+++ src/main/java/ro/unibuc/fmi/dietapp/model/Billing.java
@@ -1,6 +1,8 @@
 package ro.unibuc.fmi.dietapp.model;
 
 import lombok.*;
+import org.hibernate.annotations.NotFound;
+import org.hibernate.annotations.NotFoundAction;
 
 import javax.persistence.*;
 
@@ -17,7 +19,8 @@ public class Billing {
     @Column(name = "BILLING_ID")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @NotFound(action = NotFoundAction.IGNORE)
     private User user;
 
     @ManyToOne
